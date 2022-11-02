e215814e96ec133c843cea21a3918e5aadfdd256
Svetlana575 <Kiara575@mail.ru>
2022-11-02 11:57:41 +0300

    Lesson 3: Data types

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+// ----------------------------3. ТИПЫ ДАННЫХ--------------------------
+
+let money = 60000,
+  income = 'Eurokappa',
+  addExpenses = 'Еда, Авто, Учёба, Коммуналка',
+  deposit = false,
+  mission = 500000,
+  perlod = 11;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log('Период равен -' + ' ' + perlod + ' ' + 'месяцев.');
+// console.log(`Период равен - ${perlod} месяцев.`);
+
+
+mission = mission.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+console.log(`Цель заработать - ${mission} ₽.`);
+
+console.log(addExpenses.toLowerCase().split());
+
+let budgetDay = money / 30;
+console.log(budgetDay);
+
+
+//3*
+
+let num = 266219;
+num = num.toString().split([]);
+console.log(num);
+
+
+// Вывести в консоль произведение числа
+let newNum = num.reduce(function (a, b) {
+  return a * b;
+});
+
+console.log(newNum);
+
+// Полученный результат возвести в степень 3, используя только 1 оператор
+
+console.log(newNum ** 3);
+
+// Вывести первые 2 цифры полученного числа
+
+let numPow = newNum ** 3;
+numPow = numPow.toString().substr(0, 2);
+console.log(+numPow);
