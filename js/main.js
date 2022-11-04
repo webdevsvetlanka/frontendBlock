4f62a88a279d4ba7d341c084e0bc8ace55e0f8ad
Svetlana575 <Kiara575@mail.ru>
2022-11-04 11:37:49 +0300

    Lesson 2: Variables

--- js/main.js
+++ js/main.js
@@ -1,56 +1,63 @@
 // ----------------------------1. ЧТО ТАКОЕ JavaScript--------------------------
 
-console.log(10);
-alert('Hi, Andrey!');
+// console.log(10);
+// alert('Hi, Andrey!');
 
+
+// ----------------------------2. ПЕРЕМЕННЫЕ-------------------------
+
+const COLOR_RED = 'f00';
+let color = COLOR_RED;
+
+console.log(color);
 
 // ----------------------------3. ТИПЫ ДАННЫХ--------------------------
 
-let money = 60000,
-  income = 'Eurokappa',
-  addExpenses = 'Еда, Авто, Учёба, Коммуналка',
-  deposit = false,
-  mission = 500000,
-  perlod = 11;
+// let money = 60000,
+//   income = 'Eurokappa',
+//   addExpenses = 'Еда, Авто, Учёба, Коммуналка',
+//   deposit = false,
+//   mission = 500000,
+//   perlod = 11;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+// console.log(typeof money);
+// console.log(typeof income);
+// console.log(typeof deposit);
 
-console.log(addExpenses.length);
-console.log('Период равен -' + ' ' + perlod + ' ' + 'месяцев.');
-// console.log(`Период равен - ${perlod} месяцев.`);
-
-
-mission = mission.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-console.log(`Цель заработать - ${mission} ₽.`);
-
-console.log(addExpenses.toLowerCase().split());
-
-let budgetDay = money / 30;
-console.log(budgetDay);
+// console.log(addExpenses.length);
+// console.log('Период равен -' + ' ' + perlod + ' ' + 'месяцев.');
+// // console.log(`Период равен - ${perlod} месяцев.`);
 
 
-//3*
+// mission = mission.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+// console.log(`Цель заработать - ${mission} ₽.`);
 
-let num = 266219;
-num = num.toString().split([]);
-console.log(num);
+// console.log(addExpenses.toLowerCase().split());
+
+// let budgetDay = money / 30;
+// console.log(budgetDay);
 
 
-// Вывести в консоль произведение числа
-let newNum = num.reduce(function (a, b) {
-  return a * b;
-});
+// //3*
 
-console.log(newNum);
+// let num = 266219;
+// num = num.toString().split([]);
+// console.log(num);
 
-// Полученный результат возвести в степень 3, используя только 1 оператор
 
-console.log(newNum ** 3);
+// // Вывести в консоль произведение числа
+// let newNum = num.reduce(function (a, b) {
+//   return a * b;
+// });
 
-// Вывести первые 2 цифры полученного числа
+// console.log(newNum);
 
-let numPow = newNum ** 3;
-numPow = numPow.toString().substr(0, 2);
-console.log(+numPow);
+// // Полученный результат возвести в степень 3, используя только 1 оператор
+
+// console.log(newNum ** 3);
+
+// // Вывести первые 2 цифры полученного числа
+
+// let numPow = newNum ** 3;
+// numPow = numPow.toString().substr(0, 2);
+// console.log(+numPow);
